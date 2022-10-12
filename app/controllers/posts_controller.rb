55f5d0b1f179fe2cde4b2aeda83e895ed5ae20a1
bargholz eimii <818_77_@ezweb.ne.jp>
2022-10-12 12:08:17 +0900

    Add CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,76 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if params[:back]
+        render :new
+      else
+        if @post.save
+          format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])    
+  end
+
+  def post_params      
+    params.require(:post).permit(:image, :image_cache, :content)
+  end
+
+end
