e58d7459adc1a2bde2bbc87fcbf513f59d117b24
bargholz eimii <818_77_@ezweb.ne.jp>
2022-10-19 09:18:52 +0900

    delete post,contact mailer

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ApplicationMailer
-  def contact_mail(contact)
-    @contact = contact
-
-    mail to: @contact.email, subject: "投稿の確認メール"
-  end
-end
--- app/mailers/post_mailer.rb
+++
@@ -1,7 +0,0 @@
-# class PostMailer < ApplicationMailer
-#   def post_mail(post)
-#     @post = post
-
-#     mail to: @post.email, subject: "投稿の確認メール"
-#   end
-# end
